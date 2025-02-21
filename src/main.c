1a4a80bea1afb5edfab5fe5f913e184349da4864
Retr0-code <nikk3695@gmail.com>
2025-02-21 12:07:12 +0300

    Added process parsing

--- src/main.c
+++ src/main.c
@@ -3,8 +3,11 @@
 #include <unistd.h>
 #include <getopt.h>
 #include <string.h>
+#include <dirent.h>
+#include <linux/limits.h>
 
 int show_help(FILE *out, FILE *err) {
+    fputs("Help Menu:", out);
     fprintf(out, "--help            -h          Shows help menu\n");
     fprintf(out, "--processes       -p          Shows running processes\n");
     fprintf(out, "--users           -u          Shows users and their home dirs\n");
@@ -17,12 +20,13 @@ int show_users(FILE *out, FILE *err) {
     const char *passwd_path = "/etc/passwd";
     FILE *passwd = NULL;
     if ((passwd = fopen(passwd_path, "r")) == NULL) {
-        fprintf(err, "Unable to open %s", passwd_path);
+        fprintf(err, "Error: Unable to open %s", passwd_path);
         return -1;
     }
 
     char *line = NULL;
     size_t len = 0;
+    fputs("USERNAME HOMEDIR", out);
     while ((getline(&line, &len, passwd)) != -1) {
         static const size_t fields_amount = 5;
         char *field = NULL;
@@ -59,12 +63,63 @@ int show_users(FILE *out, FILE *err) {
 }
 
 int show_processes(FILE *out, FILE *err) {
-    
+    const char *proc_path = "/proc";
+    DIR *proc = NULL;
+    if ((proc = opendir(proc_path)) == NULL) {
+        fprintf(err, "Error: Unable to open %s", proc_path);
+        return -1;
+    }
+
+    char status_path[PATH_MAX];
+    FILE *status_file = NULL;
+    struct dirent *proc_entry = NULL;
+
+    char *line = NULL;
+    size_t len = 0;
+
+    fputs("PID PROCESSNAME", out);
+    while ((proc_entry = readdir(proc)) != NULL) {
+        if (atol(proc_entry->d_name) == 0)
+            continue;
+        
+        sprintf(status_path, "%s/%s/status", proc_path, proc_entry->d_name);
+        if ((status_file = fopen(status_path, "r")) == NULL) {
+            fprintf(err, "Error: Unable to open %s", status_path);
+            closedir(proc);
+            return -1;
+        }
+
+        if (getline(&line, &len, status_file) == -1) {
+            fprintf(err, "Error: Unable to read %s", status_path);
+            closedir(proc);
+            fclose(status_file);
+            return -1;
+        }
+        size_t prefix_len = strcspn(line, "\t") + 1;
+        fprintf(out, "%s %s", proc_entry->d_name, line + prefix_len);
+        fclose(status_file);
+    }
+    closedir(proc);
+    return 0;
+}
+
+typedef int (*function_t)(FILE *out, FILE *err);
+struct function_option_t {
+    function_t f_ptr;
+    int f_enabled;
+};
+
+
+void try_add_function(size_t *amount, struct function_option_t *functions, function_t function_ptr) {
+    if (functions[*amount].f_enabled)
+        return ;
+
+    functions[*amount].f_ptr = function_ptr;
+    functions[*amount].f_enabled = 1;
+    ++(*amount);
 }
 
 int main(int argc, char **argv) {
-    typedef int (*function_t)(FILE *out, FILE *err);
-
     static struct option arguments_long[] = {
         {"help",        no_argument,        NULL, 'h'},
         {"users",       no_argument,        NULL, 'u'},
@@ -76,8 +131,8 @@ int main(int argc, char **argv) {
     static const char *arguments_short = "hupl:e:";
     int opt = 0, option_index = 0;
 
-    function_t functions[3];
-    size_t function_amount = 0;
+    struct function_option_t functions[3];
+    size_t functions_amount = 0;
     FILE *out = stdout, *err = stderr;
 
     while ((opt = getopt_long(argc, argv, arguments_short, arguments_long, &option_index))) {
@@ -86,16 +141,13 @@ int main(int argc, char **argv) {
 
         switch (opt) {
             case 'h':
-                functions[function_amount] = &show_help;
-                ++function_amount;
+                try_add_function(&functions_amount, functions, &show_help);
                 break;
             case 'u':
-                functions[function_amount] = &show_users;
-                ++function_amount;
+                try_add_function(&functions_amount, functions, &show_users);
                 break;
             case 'p':
-                functions[function_amount] = &show_processes;
-                ++function_amount;
+                try_add_function(&functions_amount, functions, &show_processes);
                 break;
             case 'l':
                 if ((out = fopen(optarg, "w")) == NULL) {
@@ -110,16 +162,17 @@ int main(int argc, char **argv) {
                 }
                 break;
             default:
-                fprintf(err, "Error parsing arguments\n");
+                perror("Error parsing arguments\n");
                 exit(-1);
                 break;
         }
     }
 
-    for (size_t i = 0; i < function_amount; ++i)
-        (functions[i])(out, err);
+    int status = 0;
+    for (size_t i = 0; i < functions_amount; ++i)
+        status += (functions[i].f_ptr)(out, err);
 
     fclose(err);
     fclose(out);
-    return 0;
+    return status;
 }
